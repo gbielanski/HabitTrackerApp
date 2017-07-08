fab2f3cc43eb1c5e2808da57d223b61581b0c65e
gbielanski <gbielanski@poczta.onet.pl>
2017-07-08 23:41:16 +0200

    HabitDbHelper

--- app/src/main/java/pl/gbielanski/habittrackerapp/data/HabitDbHelper.java
+++ app/src/main/java/pl/gbielanski/habittrackerapp/data/HabitDbHelper.java
@@ -1,4 +1,34 @@
 package pl.gbielanski.habittrackerapp.data;
 
-public class HabitDbHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import static pl.gbielanski.habittrackerapp.data.HabitContract.HabitEntry.*;
+
+public class HabitDbHelper extends SQLiteOpenHelper{
+    private static final String SQL_CREATE_DB =
+            "CREATE TABLE " + TABLE_NAME + " (" +
+                    _ID + " INTEGER PRIMARY KEY," +
+                    COLUMN_HABIT_NAME + " TEXT NOT NULL, " +
+                    COLUMN_FREQUENCY_NAME + " INTEGER NOT NULL DEFAULT 5);";
+
+    private static final String SQL_DELETE_DB =
+            "DROP TABLE IF EXISTS " + TABLE_NAME;
+
+    private static final String DATABASE_NAME = "habit.db";
+    private static final int VERSION = 1;
+    public HabitDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_DB);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(SQL_DELETE_DB);
+    }
 }
